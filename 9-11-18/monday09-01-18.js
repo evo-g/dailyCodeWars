bd6258cec847b32703da841a94398d6e32dabad5
Evodio Garcia <evodiogarcia@Evodios-MacBook-Pro.local>
2018-10-01 17:23:03 -0700

    interview prep work

---
+++ 9-11-18/monday09-01-18.js
@@ -0,0 +1,23 @@
+function countBarks(str) {
+    var barkCounter = 0;
+    var rightCounter = 0;
+    // var strBreak = str.split('');
+    // console.log('strbreak', strBreak)
+
+    for(let i = 0; i < str.length; i++){
+        console.log('index', str[i])
+        if(str[i] === '>') {
+            rightCounter+= 1;
+            console.log('right', rightCounter)
+        } else if(str[i] === '<') {
+            barkCounter += rightCounter * 2;
+            console.log('bark', barkCounter)
+        }
+
+    }
+    console.log('total', barkCounter)
+} 
+
+// countBarks('<-<-<-<')
+ countBarks('<-<-<->-<-<-<') 
+//   countBarks('<-<-<->-<->-<-<-<')
